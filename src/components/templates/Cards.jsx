1e8b3038487874007d81904d9ec4cf3b86266378
Aman Kushwaha <amankushwaha1331@gmail.com>
2024-08-24 02:20:22 +0530

    card updated

--- src/components/templates/Cards.jsx
+++ src/components/templates/Cards.jsx
@@ -1,11 +1,20 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-
-const Cards = () => {
+const Cards = ({data}) => {
   return (
     <div>
-      CARDS
+      {data.map((d,i)=>(
+        <Link key={i}>
+          <img src={`https://image.tmdb.org/t/p/original/${d.poster_path || d.backdrop_path || d.profile_path}`} alt={d.title || d.name || d.original_title || d.original_name} />
+          <h1 className='text-xl font-bold' >{d.title || d.name || d.original_title || d.original_name}</h1>
+        </Link>
+      ))  }
     </div>
   )
 }
 
+Cards.propTypes = {
+  data: PropTypes.array.isRequired,
+}
 export default Cards
