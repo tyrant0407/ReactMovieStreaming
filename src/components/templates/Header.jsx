ee2765aa5db9a6b10680691f205ccf4f25d11af4
Aman Kushwaha <amankushwaha1331@gmail.com>
2024-11-11 02:52:25 +0530

    HeroImage Updated

--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 
 const Header = ({data}) => {
     console.log(data)
@@ -10,7 +10,12 @@ const Header = ({data}) => {
        backgroundSize: "cover"
     }} className='w-full h-[60vh] flex flex-col justify-end p-[5%]'>
         <h1 className='text-white text-5xl font-bold w-[70%]' >{data.title || data.name || data.original_title || data.original_name}</h1>
-        <p className='text-white text-xl w-[70%]' >{data.overview}</p>
+        <p className='text-[#9ca3af] text-xl w-[70%] mt-3' >{data.overview.slice(0,200)} .... <Link className='text-blue-500'>More</Link> </p>
+        <p className='text-white text-xl mt-3 flex items-center gap-2 capitalize' > 
+            <i className="text-yellow-500 ri-megaphone-fill"></i> <h1 className='text-xl font-semibold text-[#f6f9ff]' >{data.release_date || data.first_air_date} </h1>
+            <i className="text-yellow-500 ri-star-fill"></i> <h1 className='text-xl font-semibold text-[#f6f9ff]' >{data.vote_average} </h1>   
+            <i className="text-yellow-500 ri-album-fill "></i> <h1 className='text-xl font-semibold text-[#f6f9ff]' >{data.media_type} </h1>
+        </p>
         </div>
   ) 
 }
